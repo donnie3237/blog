51ebf6482aee450e1074f29841bf55f2e3e847fa
D0NN1E <paradorn3237@gmail.com>
2023-12-30 17:15:01 +0700

    .

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -9,8 +9,8 @@ interface SiteConfig {
 }
 
 export const siteConfig: SiteConfig = {
-	author: 'DanielCG', // Site author
-	title: 'Astro Theme OpenBlog', // Site title.
+	author: 'dose', // Site author
+	title: 'dose blog', // Site title.
 	description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', // Description to display in the meta tags
 	lang: 'en-GB',
 	ogLocale: 'en_GB',
