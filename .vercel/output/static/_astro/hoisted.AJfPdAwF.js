71b83719b2885e9478b21b982b4e2c7786f988e3
D0NN1E <paradorn3237@gmail.com>
2024-01-19 14:53:24 +0700

    .

---
+++ .vercel/output/static/_astro/hoisted.AJfPdAwF.js
@@ -0,0 +1,1 @@
+import{a as t}from"./hoisted.kt7b0Gcb.js";import"./hoisted.soYr7d_4.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
